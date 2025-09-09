c2ebacb8c0a08c34ebc1fdbb5aad5d81de17a67a
jj1980a <40311124+jj1980a@users.noreply.github.com>
2025-09-09 15:34:37 +0400

    feat: stream transactions on ipc endpoint (#3)
    * remove all logs scraping related functionalities
    
    * remove api
    
    * connected and listen for transactions on ipc endpoint
    
    * do not error on xxl frames
    
    * use newline delimited protocol
    
    * optimize Dockerfile, image size down to 21mb instead of 1gb
    
    * remove app user from Dockerfile
    
    * switch back to big endian 4 bytes

--- config/config.go
+++ config/config.go
@@ -6,9 +6,8 @@ import (
 )
 
 type Config struct {
-	LogLevel            string
-	HttpPort            int
-	HealthcheckEndpoint string
+	LogLevel string
+	IPCPath  string
 }
 
 func NewConfig() *Config {
@@ -16,8 +15,7 @@ func NewConfig() *Config {
 
 	fs := flag.NewFlagSet("UserConfig", flag.ExitOnError)
 	fs.StringVar(&conf.LogLevel, "log-level", "debug", "Log level")
-	fs.IntVar(&conf.HttpPort, "http-port", 8080, "HTTP port")
-	fs.StringVar(&conf.HealthcheckEndpoint, "healthcheck-endpoint", "/health", "Healthcheck endpoint")
+	fs.StringVar(&conf.IPCPath, "ipc-path", "/tmp/monad-validator.ipc", "Unix domain socket path for IPC connection")
 
 	fs.Parse(os.Args[1:])
 	return &conf
